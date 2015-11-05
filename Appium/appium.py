d008ddc930e889e5e8b08df5a49ee005f15c59dd
Json <flamecontrol@163.com>
2015-11-05 18:50:24 +0800

    Add appium

---
+++ Appium/appium.py
@@ -0,0 +1,9 @@
+from appium import webdriver
+desired_caps = {}
+desired_caps['platformName'] = 'Android'
+desired_caps['platformVersion'] = '4.2'
+desired_caps['deviceName'] = 'Android Emulator'
+desired_caps['appPackage'] = 'com.android.calculator2'
+desired_caps['appActivity'] = '.Calculator'
+
+driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
