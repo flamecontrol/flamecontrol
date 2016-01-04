db5f75a2f3806609459d6e10702e71bc62277642
gaojie <flamecontrol@163.com>
2016-01-04 18:29:19 +0800

    test

--- Appium/test_script/homepage/homepage.py
+++ Appium/test_script/homepage/homepage.py
@@ -1,7 +1,8 @@
-
+# -*- coding: utf-8 -*-
+#coding=utf-8
 import unittest
 import sys
-sys.path.append("./utils")
+sys.path.append("../../utils")
 from getScreenShot import screen
 
 s = screen()
@@ -25,6 +26,13 @@ class HOME(unittest.TestCase):
         # driver.find_elements_by_class_name("android.widget.TextView")[0].click() # 全部
         # driver.find_elements_by_class_name("android.widget.TextView")[2].click() # 评测
         # driver.find_elements_by_xpath("//android.widget.RelativeLayout/android.support.v4.view.ViewPager/android.widget.ImageView")[0].click() # 轮播图图片
-        s.snapshot("test001.jpg")
+        # s.snapshot("test001.jpg")
         # driver.keyevent(4) # 后退
 
+    def testsearch(self):
+        # driver.find_element_by_id("btn_search").click()    # 搜索
+        # driver.find_element_by_id("edittext").send_keys("汽车") # 输入有点问题
+        # driver.find_element_by_id("poptext").click() # 设置搜索类型
+        # driver.find_element_by_id("bar_right_btn").click() # 关闭搜索
+        # driver.find_elements_by_id("value")[0].click() # 第一条历史记录
+        # driver.find_element_by_id("clear").click() # 关闭搜索
